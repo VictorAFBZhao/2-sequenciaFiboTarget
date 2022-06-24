0f88761f37706d9139d1f4112a8b2e19905cd966
Victor <victor.alexanderzhao@gmail.com>
2022-06-24 11:41:00 -0300

    first commit

---
+++ TesteTarget/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TesteTarget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0;
+            int n2 = 1;
+            int soma = 0;
+            int desejado=0;
+
+            Console.WriteLine("digite um numero: ");
+            desejado = int.Parse(Console.ReadLine());
+
+            while(soma <= desejado)
+            {
+                soma = n1 + n2;
+                n1 = n2;
+                n2 = soma;
+                Console.Write("{ ");
+                Console.Write(soma);
+                Console.Write(" }");
+                Console.Write(" ---");
+            }
+          
+
+            if( n1 == desejado)
+            {
+                Console.WriteLine("O numero escolhido pertence a sequencia. ");
+            }
+            else
+            {
+                Console.WriteLine("O numero escolhido nao pertence a sequencia. ");
+            }
+        }
+    }
+}
